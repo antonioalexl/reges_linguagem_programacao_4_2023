0775f9c0ad3e66ad4d940ba2f1e7e34cd7722ebc
Alex <antonio.alex@hotmail.com>
2023-09-22 21:46:39 -0300

    aula07

--- SuperReges/src/br/com/superreges/dominio/Cliente.java
+++ SuperReges/src/br/com/superreges/dominio/Cliente.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dominio;
+package br.com.superreges.dominio;
 
 import java.util.Calendar;
 
@@ -17,6 +17,16 @@ public class Cliente extends Pessoa {
     public Cliente(String nomeCompleto, String nomeResumido, Endereco endereco, String telefone, Calendar dataNascFund, String documento, String rgIe, String email, String cartaoFidelidade) {
         super(nomeCompleto, nomeResumido, endereco, telefone, dataNascFund, documento, rgIe,email);
         this.cartaoFidelidade = cartaoFidelidade;
+    }
+    
+    
+    public void imprimir(){
+    
+         System.out.println("Nome: " + this.getNomeCompleto());
+         System.out.println("Documento: " + this.getDocumento());
+         System.out.println("Rg: " + this.getRgIe() );
+         System.out.println("Telefone: " + this.getTelefone());
+         System.out.println("Email: " + this.getEmail());
     }
     
 }
